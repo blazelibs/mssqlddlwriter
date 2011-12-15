adaf833c757623007e9825cfaf304a84eba08712
Randy Syring <rsyring@gmail.com>
2011-12-15 14:06:40 -0500

    adjust jenkins build scripts, add option to take SA URL from environ

--- mssqlddlwriter/nose_plugin.py
+++ mssqlddlwriter/nose_plugin.py
@@ -14,24 +14,35 @@ class ConfigPlugin(Plugin):
     enabled = True
 
     def options(self, parser, env=os.environ):
-        parser.add_option("--msddl-section",
-                          type="string",
-                          default=env.get('MSDDL_TEST_CONFIG', 'DEFAULT'),
-                          help="The name of the section to use from tests.cfg"
-                        )
+        parser.add_option(
+            "--msddl-section",
+            type="string",
+            default=env.get('MSDDL_TEST_CONFIG', 'DEFAULT'),
+            help="The name of the section to use from tests.cfg"
+        )
+        parser.add_option(
+            "--msddl-sa-url",
+            type="string",
+            default=env.get('MSDDL_SA_URL', None),
+            help="An SQLAlchemy connection string URL"
+        )
 
         Plugin.options(self, parser, env=env)
 
     def configure(self, options, config):
         """Configure the plugin"""
 
-        _parser.read(th.cfgpath)
-        section = options.msddl_section
+        if options.msddl_sa_url is None:
+            _parser.read(th.cfgpath)
+            print 'using config: %s' % th.cfgpath
+            section = options.msddl_section
 
-        if not _parser.has_section(section) and section != 'DEFAULT':
-            raise ValueError('the tests.cfg file does not have section: %s' % section)
+            if not _parser.has_section(section) and section != 'DEFAULT':
+                raise ValueError('the tests.cfg file does not have section: %s' % section)
 
-        th.config.sa_url = _parser.get(section, 'sa_url')
+            th.config.sa_url = _parser.get(section, 'sa_url')
+        else:
+            th.config.sa_url = options.msddl_sa_url
 
     def begin(self):
         th.engine = sa.create_engine(th.config.sa_url)
