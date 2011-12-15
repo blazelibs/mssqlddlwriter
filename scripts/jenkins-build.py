8fc01a7ebd3967385e28b108ff86091be887e903
Randy Syring <randy.syring@level12.io>
2011-12-15 14:06:40 -0500

    adjust jenkins build scripts, add option to take SA URL from environ

--- scripts/jenkins-build.py
+++ scripts/jenkins-build.py
@@ -7,13 +7,13 @@ type = 'build'
 bh = BuildHelper(package, type)
 
 # delete & re-create the venv
-bh.venv_create()
+bh.venv_create(delete_first=False)
 
 ## install package w/ setuptools develop command
 bh.vecall('python', 'setup.py', '--quiet', 'develop_with_nose')
 
 ## install pymssql
-bh.vepycall('easy_install', 'http://pymssql.googlecode.com/files/pymssql-2.0.0b1_dev_20111018-py2.6-linux-x86_64.egg')
+#bh.vepycall('easy_install', 'http://pymssql.googlecode.com/files/pymssql-2.0.0b1_dev_20111018-py2.6-linux-x86_64.egg')
 
 ## run tests
-bh.vepycall('nosetests', 'mssqlddlwriter', '--with-xunit')
+bh.vepycall('nosetests', 'mssqlddlwriter', '--with-xunit', '-s')
