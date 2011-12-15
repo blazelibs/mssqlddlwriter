620d2d0d9ed4b8253632824ff1f9a6bc979d0243
Randy Syring <rsyring@gmail.com>
2011-12-15 12:43:31 -0500

    add jenkins build script

--- scripts/jenkins-build.py
+++ scripts/jenkins-build.py
@@ -10,7 +10,10 @@ bh = BuildHelper(package, type)
 bh.venv_create()
 
 ## install package w/ setuptools develop command
-bh.setuppy_develop()
+bh.vecall('python', 'setup.py', '--quiet', 'develop_with_nose')
+
+## install pymssql
+bh.vepycall('easy_install', 'http://pymssql.googlecode.com/files/pymssql-2.0.0b1_dev_20111018-py2.6-linux-x86_64.egg')
 
 ## run tests
 bh.vepycall('nosetests', 'mssqlddlwriter', '--with-xunit')
